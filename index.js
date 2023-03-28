ff6a9ba0f78ac504e446b2eb9c24c000b7e1d98b
unknown <gordoncarters97@yahoo.com>
2023-03-28 16:21:43 -0700

    removed buggy file

--- index.js
+++
@@ -1,7 +0,0 @@
-alert("hrllo");
-console.log("hell2");
-
-function ll(form) {
-    if (form.value == "One Lap") window.location.replace("index.php?r=0");
-    else if (form.value == "Three Laps") window.location.replace("index.php?r=1");
-}
