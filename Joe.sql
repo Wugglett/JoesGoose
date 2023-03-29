b13bb9455a8a264db5e3c9b7403be637baa8c6e3
unknown <gordoncarters97@yahoo.com>
2023-03-29 15:33:33 -0700

    Added Comments table to SQL databse

--- Joe.sql
+++ Joe.sql
@@ -19,6 +19,18 @@ CREATE TABLE Runs (
     PRIMARY KEY(id)
 );
 
+CREATE TABLE Comments (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    user_id BIGINT NOT NULL,
+    run_id BIGINT,
+    comment_id BIGINT,
+    content VARCHAR(255) NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES Users(id),
+    FOREIGN KEY(run_id) REFERENCES Runs(id),
+    PRIMARY KEY(id),
+    CONSTRAINT require_parent_id CHECK((run_id IS NULL OR comment_id IS NULL) AND NOT (run_id IS NULL AND comment_id IS NULL)) -- Did not work need to implement later
+);
+
 CREATE TABLE Tokens (
 	token VARCHAR(255) NOT NULL,
     user_id BIGINT NOT NULL
