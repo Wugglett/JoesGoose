74b69b2a765b924020da3e142eb186b89e795ccd
unknown <gordoncarters97@yahoo.com>
2023-04-19 17:30:34 -0700

    Started working on picture verification for profile image file

---
+++ profile_page.js
@@ -0,0 +1,8 @@
+function isFileImage(file) {
+    // check if file is an image
+
+    // if so Enable submit button
+
+    // if not disable submit button and print error
+    alert("hello");
+}
--- profile_page.php
+++ profile_page.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <script src="profile_page.js"></script>
     <title>Joe's User Profile Race</title>
 </head>
 <body>
@@ -91,12 +92,14 @@
         }
 
         if (isset($_SESSION["user_id"]) && $_SESSION["user_id"] == $row['id']) {
-            printf("<div class=\"text-light\"><form action=\"profile_picture.php\" method=\"post\" enctype=\"multipart/form-data\">
+            printf("<div class=\"text-light mt-3\">
+            <form action=\"profile_picture.php\" method=\"post\" enctype=\"multipart/form-data\">
             <label>%s Profile Picture:</label>
-            <input type=\"file\" name=\"image\" id=\"image\">
+            <input class=\"form-control form-control-sm bg-dark text-light\" type=\"file\" name=\"image\" id=\"image\" onchange=\"isFileImage(file)\">
             <input type=\"hidden\" name=\"username\" id=\"username\" value=\"%s\">
-            <input type=\"submit\" name=\"submit\" value=\"Upload\">
-            </form></div>",$picture_set, $_GET["u"]);
+            <input class=\"btn btn-sm btn-secondary mt-2\" type=\"submit\" name=\"submit\" id=\"submit\" value=\"Upload\">
+            </form>
+            </div>",$picture_set, $_GET["u"]);
         }
     ?>
             </div>
@@ -189,5 +192,4 @@
     <div class="col-lg-1"></div>
     </div>
 </body>
-    <script src="index.js"></script>
 </html>
